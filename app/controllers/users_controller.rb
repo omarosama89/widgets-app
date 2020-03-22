51678dbe6ee9a305d3a21910e7f73b9b1972256d
Omar <o.mohammed@morniksa.com>
2020-03-22 21:35:13 +0200

    add change password functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
-  def new
-
-  end
+  
   def create
     res = ShowoffApiWrapper.post('/api/v1/users', {user: user_params}, "Content-Type" => 'application/json')
     if res['code'] == 0
@@ -22,6 +20,18 @@ class UsersController < ApplicationController
     end
   end
 
+  def change_password
+    res = ShowoffApiWrapper.post('/api/v1/users/me/password', {user: change_password_params}, {"Content-Type" => 'application/json', 'Authorization' => "#{session[:current_user]['token']['token_type']} #{session[:current_user]['token']['access_token']}"})
+    if res['code'] == 0
+      flash[:success] = 'Password have been changed successfully. You can Login using the new password'
+      unset_curent_user!
+      redirect_to widgets_path, status: 301
+    else
+      flash[:danger] = res['message']
+      render :new_change_password
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :password, :email, :image_url)
@@ -29,5 +39,9 @@ class UsersController < ApplicationController
 
   def user_update_params
     params.require(:user).permit(:first_name, :last_name, :date_of_birth, :image_url)
+  end
+
+  def change_password_params
+    params.require(:user).permit(:current_password, :new_password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,12 @@ Rails.application.routes.draw do
       post :refresh
     end
   end
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create] do
+    collection do
+      get :new_change_password
+      post :change_password
+    end
+  end
   resources :widgets, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

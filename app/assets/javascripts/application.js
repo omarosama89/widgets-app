8a3787889b2b3519d520a4325f63b94d3b3a6f70
Omar <o.mohammed@morniksa.com>
2020-03-23 23:59:57 +0200

    fix including bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,36 +12,72 @@
 //
 //= require jquery
 
-//= require bootstrap
-//= require turbolinks
-//= require app
+
+//= require jquery
 //= require jquery_ujs
-//= require_tree .
+//= require back/plugins/bootstrap/bootstrap.min
+//= require back/plugins/bootstrap-tagsinput/bootstrap-tagsinput.min
+//= require back/plugins/datatables/jquery.dataTables.min
+//= require back/plugins/datatables/dataTables.bootstrap.min
+//= require back/app
+//= require_self
 
-$('#email-field').focusout(function(){
+
+$(document).ready(function() {
+    var table = $('.dataTable').DataTable();
+
+    // Get sidebar state from localStorage and add the proper class to body
+    $('body').addClass(localStorage.getItem('sidebar-state'));
+
+    var activePage = stripTrailingSlash(window.location.pathname);
+    $('.sidebar-menu li a').each(function(){
+        var currentPage = stripTrailingSlash($(this).attr('href'));
+        if (activePage == currentPage) {
+            $(this).closest('.treeview').addClass('active');
+            $(this).parent().addClass('active');
+        }
+    });
+    function stripTrailingSlash(str) {
+        if(str.substr(-1) == '/') { return str.substr(0, str.length - 1); }
+        return str;
+    }
+
+    // Save sidebar state in localStorage browser
+    $('.sidebar-toggle').on('click', function(){
+        if($('body').attr('class').indexOf('sidebar-collapse') != -1) {
+            localStorage.setItem('sidebar-state', '');
+        } else {
+            localStorage.setItem('sidebar-state', 'sidebar-collapse');
+        }
+    });
+    $('#email-field').focusout(function(){
         $.get('/api/users/check_email', {user: {email: $('#email-field').val()}}, function(data, status){
-        $('#check-email-status').empty();
-        if(data.data.available){
-            $('#check-email-status').append('<div class="alert alert-success">Email is available</div>');
-            $('#signup-button').removeAttr('disabled')
-        }
-        else{
-            $('#check-email-status').append('<div class="alert alert-danger">Email is not available</div>');
-            $('#signup-button').attr('disabled', 'disabled')
-        }
-    })
+            $('#check-email-status').empty();
+            if(data.data.available){
+                $('#check-email-status').append('<div class="alert alert-success">Email is available</div>');
+                $('#signup-button').removeAttr('disabled')
+            }
+            else{
+                $('#check-email-status').append('<div class="alert alert-danger">Email is not available</div>');
+                $('#signup-button').attr('disabled', 'disabled')
+            }
+        })
+    });
+
+    $('#reset-password-email-field').focusout(function(){
+        $.get('/api/users/check_email', {user: {email: $('#reset-password-email-field').val()}}, function(data, status){
+            $('#reset-password-email-status').empty();
+            if(data.data.available){
+                $('#reset-password-email-status').append('<div class="alert alert-danger">Email is not valid</div>');
+                $('#reset-password-button').attr('disabled', 'disabled')
+            }
+            else{
+                $('#reset-password-email-status').append('<div class="alert alert-success">Email is valid</div>');
+                $('#reset-password-button').removeAttr('disabled')
+            }
+        })
+    });
+
 });
 
-$('#reset-password-email-field').focusout(function(){
-    $.get('/api/users/check_email', {user: {email: $('#reset-password-email-field').val()}}, function(data, status){
-        $('#reset-password-email-status').empty();
-        if(data.data.available){
-            $('#reset-password-email-status').append('<div class="alert alert-danger">Email is not valid</div>');
-            $('#reset-password-button').attr('disabled', 'disabled')
-        }
-        else{
-            $('#reset-password-email-status').append('<div class="alert alert-success">Email is valid</div>');
-            $('#reset-password-button').removeAttr('disabled')
-        }
-    })
-});
+
